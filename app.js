bfeb6ccb7cb8523bb9390f94741c2fedca7d4b87
Mariya Setko <mariya.setko@gmail.com>
2022-10-13 13:41:41 -0400

    flipCard function

--- app.js
+++ app.js
@@ -50,3 +50,33 @@ const cardArray = [
     }
 ]
 
+const grid = document.querySelector.apply('.grid')
+var cardsChosen = []
+var cardsChosenId = []
+
+//create board
+function createBoard() {
+    for (let i = 0; i < cardArray.length; i++) {
+        var card = document.createElement('img')
+        card.setAttribute('src', 'images/blank.png')
+        card.setAttribute('data-id', i)
+        //card.addEventListener('click', flipCard)
+        grid.appendChild(card)
+    }
+}
+
+//check for matches
+
+
+//flip your card
+function flipCard() {
+    var cardId = this.getAttribute('data-id')
+    cardsChosen.push(cardArray[cardId].name)
+    cardsChosenId.push(cardId)
+    this.setAttribute('src', cardArray[cardId].img)
+    if (cardsChosen.length === 2) {
+        setTimeout(checkForMatch, 500)
+    }
+}
+
+createBoard()
